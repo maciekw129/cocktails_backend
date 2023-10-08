ede971fcd5e5e8e9a45a202a8e085fbed3871892
Maciej Walecki <maciejwalecki99@gmail.com>
2023-10-08 16:59:53 +0200

    feat: added env cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1/');
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [process.env.FRONT_URL],
     methods: ['GET', 'POST', 'PATCH'],
   });
   await app.listen(10000);
