621196639436280a9ce5e000b344eb2950d54f12
Maciej Walecki <maciejwalecki99@gmail.com>
2023-10-10 21:03:20 +0200

    feat: modified bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import {ConfigService} from "@nestjs/config";
+import * as console from "console";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  const port = configService.get<string>('port');
+  const frontUrl = configService.get<string>('frontUrl');
+
+  console.log(`App is running on port: ${port}`);
+  console.log(`Allowed cors for: ${frontUrl}`);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1/');
   app.enableCors({
-    origin: [configService.get<string>('frontUrl')],
+    origin: [frontUrl],
     methods: ['GET', 'POST', 'PATCH'],
   });
-  await app.listen(configService.get<string>('port'));
+  await app.listen(port);
 }
 bootstrap();
